66e5373f288a427f12325526ee44c6024958bb1e
Will Nilges <wdn5796@rit.edu>
2023-03-23 22:30:25 -0400

    Explicit commands in configure script

--- idiot/configure.sh
+++ idiot/configure.sh
@@ -7,8 +7,8 @@ cd /home/devcade
 wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
-apt-get update && \
-  apt-get install -y dotnet-sdk-6.0
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
 
 # Set up Onboard
 cd devcade-onboard;
@@ -16,15 +16,16 @@ cd devcade-onboard;
 # Build onboard
 cd onboard
 dotnet publish -c release -r linux-x64 --self-contained
+rm -rf /home/devcade/publish
 mv bin/Release/net6.0/linux-x64/publish /home/devcade
 cd ..
 
 # TODO: Set up publish.sh? IDK man it should work fine as is.
 
-cp idiot/.env /home/devcade; 
-cp idiot/xinitrc ~/.xinitrc;
+cp /home/devcade/idiot/.env /home/devcade; 
+cp /home/devcade/idiot/xinitrc ~/.xinitrc;
 git submodule update --init --recursive;
-cd idiot/xlogin;
-make install;
-systemctl enable xlogin@devcade;
+cd /home/devcade/idiot/xlogin;
+sudo make install;
+sudo systemctl enable xlogin@devcade;
 
