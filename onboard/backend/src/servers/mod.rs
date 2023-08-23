314d0d901c997df12eb3d2d690684830cc02abf1
joe <apjoey914@gmail.com>
2023-08-23 17:25:53 -0400

    Fix panic loop when restarting backend

--- onboard/backend/src/servers/mod.rs
+++ onboard/backend/src/servers/mod.rs
@@ -1,7 +1,10 @@
+use anyhow::anyhow;
 use futures_util::future;
 use futures_util::FutureExt;
 use log::{log, Level};
+use std::fs::remove_file;
 use std::future::Future;
+use std::process::{Command, Stdio};
 use std::sync::Arc;
 use tokio::io::{AsyncBufReadExt, BufReader, Lines, ReadHalf, WriteHalf};
 use tokio::net::{UnixListener, UnixStream};
@@ -132,7 +135,7 @@ where
         + 'a + 'static,
     U: Future<Output = Result<(), anyhow::Error>> + Send + Sync + 'a + 'static,
 {
-    let listener = UnixListener::bind(path).unwrap();
+    let listener = bind_listener(path).unwrap();
     let handle_client = Arc::new(handle_client);
 
     let mut handles = vec![];
@@ -150,4 +153,29 @@ where
     }
     future::join_all(handles).await;
     panic!("Looks like our server stopped serving?! This shouldn't happen.");
+}
+
+fn bind_listener(path: &str) -> Result<UnixListener, anyhow::Error> {
+    match UnixListener::bind(path) {
+        Ok(l) => Ok(l),
+        Err(e) => match e.to_string().as_str() {
+            "Address already in use (os error 98)" => {
+                // Using std::process::Command instead of tokio equivalent because lsof doesn't
+                // take long enough to bother
+                let lsof = Command::new("lsof")
+                    .arg(path)
+                    .stdout(Stdio::null())
+                    .status()?;
+                if lsof.success() {
+                    // lsof returns success if any process if using this file
+                    Err(anyhow!("Failed to bind listener to path {}: {}", path, e))
+                } else {
+                    log::debug!("Socket was not closed correctly in last shutdown. Removing");
+                    remove_file(path)?;
+                    bind_listener(path)
+                }
+            }
+            _ => Err(anyhow!("Failed to bind listener to path {}: {}", path, e)),
+        },
+    }
 }
