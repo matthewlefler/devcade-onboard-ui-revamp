0b2821b2351b41ad5133f1ac4245ee9903572bdc
matthewlefler <matthewlefler1@gmail.com>
2025-03-31 21:20:22 -0400

    removed original frontend folder, copied original assets into CSHAssets folder

--- onboard/frontend/release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dotnet publish -c Release -r linux-x64 --no-self-contained && \
-scp -r /Devcade-onboard/onboard/bin/Release/netcoreapp3.1/linux-x64/publish/ devcade@devcade.csh.rit.edu:~/onboard-new
-
--- onboard/frontend/to_linux.ps1
+++
@@ -1,3 +0,0 @@
-dotnet publish -c Release -r linux-x64 --no-self-contained
-rm -r -force C:\Users\dingus\Documents\devcade-shared\publish\
-cp C:\Users\dingus\Code\Devcade-onboard\onboard\bin\Release\netcoreapp3.1\linux-x64\publish\ C:\Users\dingus\Documents\devcade-shared\ -r
