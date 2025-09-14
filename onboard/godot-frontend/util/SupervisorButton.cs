d8759498e1083b027f2335c8f6a4f95aa9a9493d
matthewlefler <matthewlefler1@gmail.com>
2025-09-14 19:59:34 -0400

    added supervisorButton class and working logic for supervisor button

---
+++ onboard/godot-frontend/util/SupervisorButton.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+namespace onboard.util.supervisor_button
+{
+    static class SupervisorButton
+    {
+        public static bool anyButtonPressed { get { return Input.IsAnythingPressed(); } private set { } }
+
+        public static bool isSupervisorButtonPressed()
+        {
+            bool player1_menu_pressed;
+            bool player2_menu_pressed;
+
+            // these are hard coded values that should be changed to something else
+            player1_menu_pressed = Input.IsJoyButtonPressed(0, JoyButton.Start);
+            player2_menu_pressed = Input.IsJoyButtonPressed(1, JoyButton.Start);
+
+            return player1_menu_pressed && player2_menu_pressed;
+        } 
+    }
+}
