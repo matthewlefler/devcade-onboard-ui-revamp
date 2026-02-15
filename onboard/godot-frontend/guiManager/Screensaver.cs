fd5db4021734623f61342fca3a21c406c8857994
matthewlefler <matthewlefler1@gmail.com>
2026-02-14 19:50:29 -0500

    new screensaver framework

--- onboard/godot-frontend/guiManager/Screensaver.cs
+++ onboard/godot-frontend/guiManager/Screensaver.cs
@@ -1,6 +1,90 @@
-using Godot;
 using System;
+using System.Collections.Generic;
+using Godot;
 
 public partial class Screensaver : Control
 {
+    [Export]
+    public float scrollSpeed = 1.0f;
+
+    [Export]
+    private Control gamesAnimationsContainer;
+
+    [Export]
+    private TextureRect backgroundIcons;
+    
+    private List<ScreenSaverGameAnimation> gameAnimationNodes = new List<ScreenSaverGameAnimation>();
+
+    private bool playing = false;
+    private int currentGameAnimationIndex = 0;
+
+    Vector2 startPosition;
+    Vector2 endPosition;
+    float screenWidth;
+
+    public void play()
+    {
+        playing = true;   
+        currentGameAnimationIndex = 0;
+        gamesAnimationsContainer.Position = startPosition;
+        shader_offset = 0.0f;
+    }
+
+    public void stop()
+    {
+        playing = false;
+    }
+
+    public override void _Ready()
+    {
+        screenWidth = GetViewportRect().Size.X;
+
+        startPosition = new Vector2(0, 0);
+
+        foreach(Node node in gamesAnimationsContainer.GetChildren())
+        {
+            if(node is ScreenSaverGameAnimation gameAnimation)
+            {
+                gameAnimationNodes.Add(gameAnimation);
+                gameAnimation.Position = startPosition;
+            }
+            else
+            {
+                GD.PrintErr($"found child node {node.Name} that is not a ScreenSaverGameAnimation");
+            }
+        }
+
+        endPosition = new Vector2(-1 * screenWidth * (gameAnimationNodes.Count + 1), 0);
+
+        for (int i = 0; i < gameAnimationNodes.Count; i++)
+        {
+            ScreenSaverGameAnimation anim = gameAnimationNodes[i];
+
+            anim.Position = new Vector2(screenWidth * (i + 1), 0);
+        }
+    }
+
+    float shader_offset = 0.0f;
+    public override void _Process(double delta)
+    {
+        if(!playing)
+        {
+            return;
+        }
+
+        shader_offset += scrollSpeed / 230.0f;
+        setShaderXOffest(shader_offset);
+
+        gamesAnimationsContainer.Position += new Vector2(-scrollSpeed, 0);
+
+        if(gamesAnimationsContainer.Position.X < endPosition.X)
+        {
+            gamesAnimationsContainer.Position = startPosition;
+        }
+    }
+
+    private void setShaderXOffest(float x)
+    {
+        backgroundIcons.Material.Set("shader_parameter/x_offset", x);
+    }
 }
---
+++ onboard/godot-frontend/guiManager/screensaver_games_animations/ScreenSaverGameAnimation.cs
@@ -0,0 +1,7 @@
+using Godot;
+
+public partial class ScreenSaverGameAnimation : Control
+{
+    [Export]
+    public AnimatedSprite2D animation;
+}
