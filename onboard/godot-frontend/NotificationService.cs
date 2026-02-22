91c4401f60c878fa58b7969f60361802420e0eb2
matthewlefler <matthewlefler1@gmail.com>
2026-02-21 20:38:43 -0500

    test runs on my machine

--- onboard/godot-frontend/NotificationService.cs
+++ onboard/godot-frontend/NotificationService.cs
@@ -1,28 +1,67 @@
+using System.Diagnostics;
 using Godot;
 
 public partial class NotificationService : Node
 {
-    [Export]
-    private string NotificationServicePath = "/home/skye/gitrepos/devcade-onboard-ui-revamp/onboard/notifications/project.godot";
-    private static readonly int failedToStart = -1;
-    private int proccesId;
-    public override void _EnterTree()
+    private Process notificationService;
+    public override void _Ready()
+    {      
+        string command = "~/.devcade/notification-service";
+        var escapedArgs = command.Replace("\"", "\\\""); // Escape double quotes within the command (" become \")
+        
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = "/bin/bash", // Specify the bash executable
+            Arguments = $"-c \"{escapedArgs}\"", // Pass the command using the -c option
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false, // Must be false to redirect streams
+            CreateNoWindow = false
+
+        };
+
+        notificationService = new Process
+        {
+            StartInfo = startInfo
+        };
+
+        notificationService.OutputDataReceived += (sender, args) =>
+        {
+            if (args.Data != null)
+                GD.Print($"notification-service: {args.Data}");
+        };
+
+        notificationService.ErrorDataReceived += (sender, args) =>
+        {
+            if (args.Data != null)
+                GD.PushError($"notification-service: {args.Data}");
+        };
+
+        bool started = notificationService.Start();
+        notificationService.BeginOutputReadLine();
+        notificationService.BeginErrorReadLine();
+
+        if(!started)
+        {
+            GD.PushError("Failed to start Notification Service");
+        }
+        else
+        {
+            GD.Print("Suceeded in starting Notification Service");
+        }
+    }
+
+    public override void _Process(double delta)
     {
         
-
-        string[] args = {};
-
-        proccesId = OS.CreateProcess($"godot-mono -t -w --path {NotificationServicePath}", args, openConsole: false);
-
-        if(proccesId == failedToStart)
-        {
-            GD.PrintErr("Failed to start Notification Service");
-            return;
-        }
     }
 
     public override void _ExitTree()
     {
-        OS.Kill(proccesId);
+        if(notificationService != null)
+        {
+            notificationService.Kill();   
+        }
     }
+
 }
