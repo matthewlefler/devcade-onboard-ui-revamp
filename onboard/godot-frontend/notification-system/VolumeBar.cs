267e1b9dc3f291432e278ddd935716860e7d07e1
matthewlefler <matthewlefler1@gmail.com>
2026-02-26 02:35:07 -0500

    added show/hide with linger time and reduced polling rate to 5 times a sec

--- onboard/godot-frontend/notification-system/VolumeBar.cs
+++ onboard/godot-frontend/notification-system/VolumeBar.cs
@@ -3,7 +3,7 @@ using Godot;
 
 public partial class VolumeBar : ProgressBar
 {
-    Process process = new Process {
+    private Process process = new Process {
         StartInfo = new ProcessStartInfo
         {
             FileName = "pamixer",
@@ -16,9 +16,47 @@ public partial class VolumeBar : ProgressBar
         EnableRaisingEvents = true
     };
 
+    private int last_volume = -1;
+
+    [Export]
+    double secondsBetweenPolls = 0.2;
+    private double seconds = 0.0;
+    
+    [Export]
+    double lingerTime = 1.0;
+    private double lingerSec = 0.0;
+    public override void _Ready()
+    {
+        this.Visible = false;
+    }
+
     public override void _Process(double delta)
     {
-        this.Value = (float) getVolume() / 100.0f;
+        seconds += delta;
+        lingerSec += delta;
+
+        if(lingerSec > lingerTime)
+        {
+            this.Visible = false;
+        }
+        
+        if(seconds < secondsBetweenPolls)
+        {
+            return;
+        }
+        seconds = 0.0;
+
+        int volume = getVolume();
+
+        if(volume != last_volume)
+        {
+            this.Visible = true;
+            lingerSec = 0.0;
+        }
+
+        last_volume = volume;
+
+        this.Value = getVolume() / 100.0f;
     }
 
     private int getVolume()
