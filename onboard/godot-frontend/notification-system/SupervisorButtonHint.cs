79a485a1433fddedfc5a77f6717f6f50d00976d3
matthewlefler <matthewlefler1@gmail.com>
2026-03-02 00:04:49 -0500

    show supervisor hint after time spent inactive

---
+++ onboard/godot-frontend/notification-system/SupervisorButtonHint.cs
@@ -0,0 +1,31 @@
+using Godot;
+using onboard.util;
+using System;
+
+public partial class SupervisorButtonHint : MarginContainer
+{
+    [Export]
+    private double inactiveMaxTime = 60.0; // time in seconds
+    double time = 0.0;
+
+    public override void _Ready()
+    {
+        this.Hide();
+    }
+
+    public override void _Process(double delta)
+    {
+        if(Input.IsAnythingPressed())
+        {
+            time = 0.0;
+            this.Hide();
+            return;
+        }
+
+        time+=delta;
+        if(time >= inactiveMaxTime)
+        {
+            this.Show();
+        }
+    }
+}
--- onboard/godot-frontend/util/Env.cs
+++ onboard/godot-frontend/util/Env.cs
@@ -7,6 +7,30 @@ namespace onboard.util;
 
 public static class Env {
     private static readonly Dictionary<string, string> env = new();
+
+    // Logging level for the backend
+    // Allowed log levels: trace, debug, info, warn, error 
+    public static Option<string> RUST_LOG() { return get("RUST_LOG"); }
+    public static Option<string> DEVCADE_API_DOMAIN() { return get("DEVCADE_API_DOMAIN"); }
+    public static Option<string> DEVCADE_DEV_API_DOMAIN() { return get("DEVCADE_DEV_API_DOMAIN"); }
+    
+    // Frontend 
+    // Logging level for the frontend 
+    // Allowed log levels: trace, verbose, debug, info, warn, error, fatal 
+    public static Option<string> FRONTEND_LOG() { return get("FRONTEND_LOG"); }
+    // Amount of time in seconds until the screen saver is shown
+    public static int SCREENSAVER_TIMEOUT_SEC() { return int.Parse( get("SCREENSAVER_TIMEOUT_SEC").unwrap_or("300") ); }
+    // Amount of time in seconds that the supervisor buttons need to be heldW
+    public static int SUPERVISOR_BUTTON_TIMEOUT_SEC() { return int.Parse( get("SUPERVISOR_BUTTON_TIMEOUT_SEC").unwrap_or("5") ); }
+
+    // Demo mode will not display games with certain tags (e.g. "CSH Only") 
+    // Allowed values: true, false 
+    public static bool DEMO_MODE() { return get("DEMO_MODE").unwrap_or("true") == "true" ? true : false; }
+
+    // Shared 
+    // Games data and shared sockets will be placed here 
+    public static Option<string> DEVCADE_PATH() { return get("DEVCADE_PATH"); }
+
     
     static Env() {
         foreach (DictionaryEntry entry in Environment.GetEnvironmentVariables()) {
