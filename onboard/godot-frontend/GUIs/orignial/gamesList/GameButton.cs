5561f6fa6fa24e86592891a1411da75e26c23142
matthewlefler <matthewlefler1@gmail.com>
2026-04-23 16:15:51 -0400

    different interpolation function for game buttons

--- onboard/godot-frontend/GUIs/orignial/gamesList/GameButton.cs
+++ onboard/godot-frontend/GUIs/orignial/gamesList/GameButton.cs
@@ -1,10 +1,18 @@
 using Godot;
 
 // used for animations
-public partial class GameButton
+public class GameButton
 {
+	[Export]
+	public float minimumRotationSpeed = 3.0f;
+	[Export]
+	public float maxRotationSpeed = 20.0f;
+
 	public BaseButton childButton;
-	// how close this button can be to the target rotation before it snaps to the target rotation
+
+	/// <summary>
+	/// how close this button can be to the target rotation before it snaps to the target rotation
+	/// </summary>
 	const float errorMargin = 0.05f;
 	public float targetRotation; 
 
@@ -21,14 +29,19 @@ public partial class GameButton
 
 	public void process(double delta)
 	{
+		float deltaRotation = targetRotation - this.childButton.Rotation;
+		float direction = float.Sign(deltaRotation);
 
-		if(!(targetRotation - this.childButton.Rotation < errorMargin && targetRotation - this.childButton.Rotation > -errorMargin))
+		float d = (float) (1.0 + -1.0 / (1.0 + 0.13 * float.Abs(deltaRotation)));
+		float rotationSpeed = float.Lerp(minimumRotationSpeed, maxRotationSpeed, d);
+
+		if(deltaRotation < errorMargin && deltaRotation > -errorMargin)
 		{
-			this.childButton.Rotation += (targetRotation - this.childButton.Rotation) * 20f * (float) delta;
+			this.childButton.Rotation = targetRotation;
 		}
 		else
 		{
-			this.childButton.Rotation = targetRotation;
+			this.childButton.Rotation += (float) (direction * rotationSpeed * delta);
 		}
 
 		if(this.childButton.Rotation > 3.2f || this.childButton.Rotation < -3.2f) 
