2aa69719607363d20dbbb67aa5f2c87bc85d1ebd
matthewlefler <matthewlefler1@gmail.com>
2026-06-17 13:36:21 -0400

    append devcade_ to frontend and backend, move exes to /usr/bin or global config spot, edit dockerfiles so that nvidia one just builds on regular one

--- dcu/build-nvidia.sh
+++ dcu/build-nvidia.sh
@@ -1,2 +1,3 @@
+podman build --build-arg-file=./.env -t dcu-devcade-onboard:latest . && \
 podman build -f ./Dockerfile.nvidia --build-arg-file=./.env -t dcu-devcade-onboard:latest-nvidia .
 # builds the container
